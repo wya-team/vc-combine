16e41109c93b4dfbd15371528e9b75f48649a5cb
蒋东 <852778359@qq.com>
2022-02-15 14:46:27 +0800

    fix: 导出

--- packages/index/components.ts
+++ packages/index/components.ts
@@ -1,3 +1,4 @@
 export { default as Paging } from '@wya/vcc-paging';
 export { default as SetTitle } from '@wya/vcc-set-title';
 export { default as SectionTitle } from '@wya/vcc-section-title';
+export { default as Footer } from '@wya/vcc-footer';
