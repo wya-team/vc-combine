7f6a893ad086c9e86d6397782c167c53e7773626
ScoutYin <scoutyin.1900@gmail.com>
2022-02-17 21:44:38 +0800

    feat: 更新示例

--- packages/paging/examples/filter.vue
+++ packages/paging/examples/filter.vue
@@ -1,6 +1,6 @@
 <template>
 	<div style="padding: 20px;">
-		<vcc-paging-filter :modules="modules" history />
+		<vcc-paging-filter :modules="modules" :outer-count="2" history />
 	</div>
 </template>
 <script>
@@ -16,42 +16,85 @@ export default defineComponent({
 		const modules = ref([
 			{
 				type: 'input',
-				label: '关键词',
+				label: '关键词：',
 				field: 'input',
 				// 将会绑定在vc-input组件上的属性
 				options: {
 					placeholder: '请输入关键词进行搜索',
 				}
 			},
+			// {
+			// 	type: 'select',
+			// 	label: '下拉选择项：',
+			// 	field: 'select',
+			// 	dataSource: [
+			// 		{ label: '选项一', value: '1' },
+			// 		{ label: '选项二', value: '2' }
+			// 	]
+			// },
 			{
 				type: 'select',
-				label: '下拉选择项',
-				field: 'select',
-				dataSource: [
-					{ label: '选项一', value: 1 },
-					{ label: '选项二', value: 2 }
-				]
+				label: '搜索型下拉选择项：',
+				field: 'searchableSelect',
+				options: {
+					searchable: true
+				},
+				dataSource: () => {
+					return new Promise((resolve) => {
+						// 模拟异步获取
+						setTimeout(() => {
+							resolve([
+								{ label: '选项一', value: '1' },
+								{ label: '选项二', value: '2' }
+							]);
+						}, 1000);
+					});
+				}
 			},
 			{
 				type: 'cascader',
-				label: '级联选择',
+				label: '级联选择：',
 				field: 'cascader',
 				dataSource: [
-					{ label: '选项一', value: 1 },
-					{ label: '选项二', value: 2 },
+					{ label: '选项一', value: '1' },
+					{ label: '选项二', value: '2' },
 					{ 
 						label: '选项三',
 						value: 3,
 						children: [
-							{ label: '选项三 - 1', value: 31 },
-							{ label: '选项三 - 2', value: 32 }    
+							{ label: '选项三 - 1', value: '31' },
+							{ label: '选项三 - 2', value: '32' }    
 						]
 					}
 				]
 			},
 			{
+				type: 'cascader',
+				label: '异步级联选择：',
+				field: 'asyncCascader',
+				dataSource: () => {
+					return new Promise((resolve) => {
+						// 模拟异步获取
+						setTimeout(() => {
+							resolve([
+								{ label: '选项一', value: '1' },
+								{ label: '选项二', value: '2' },
+								{ 
+									label: '选项三',
+									value: 3,
+									children: [
+										{ label: '选项三 - 1', value: '31' },
+										{ label: '选项三 - 2', value: '32' }    
+									]
+								}
+							]);
+						}, 1000);
+					});
+				}
+			},
+			{
 				type: 'datePicker',
-				label: '时间/日期选择',
+				label: '时间/日期选择：',
 				field: 'datePicker',
 				// 将会绑定在vc-date-picker组件上的属性
 				// options: {
@@ -60,7 +103,7 @@ export default defineComponent({
 			},
 			{
 				type: 'rangeDatePicker',
-				label: '时间/日期范围选择',
+				label: '时间/日期范围选择：',
 				// field: ['rangeDatePicker_start', 'rangeDatePicker_end'],
 				field: 'rangeDatePicker',
 				// 将会绑定在vc-date-picker组件上的属性
@@ -70,19 +113,20 @@ export default defineComponent({
 			},
 			{
 				type: 'range',
-				label: '整数/小数范围',
+				label: '整数/小数范围：',
 				children: [
 					{
 						field: 'rangeStart',
 						// 将会绑定在vc-input-number组件上的属性
 						options: {
-							max: 2,
+							// precision: 2,
+							max: 100,
 						}
 					},
 					{
 						field: 'rangeEnd',
 						options: {
-							max: 2,
+							max: 100,
 						}
 					}
 				]
@@ -92,17 +136,37 @@ export default defineComponent({
 				children: [
 					{
 						field: 'selectComboType',
+						defaultValue: '1',
 						dataSource: [
 							{ label: '选项一', value: '1' },
 							{ label: '选项二', value: '2' }
 						],
+						style: 'margin-left:24px',
 						// 将会绑定在vc-select组件上的属性
 						// options: {}
 					},
 					{
-						type: 'rangeDatePicker',
-						field: 'selectComboFilter',
+						type: 'range',
+						children: [
+							{
+								field: 'combo_rangeStart',
+								// 将会绑定在vc-input-number组件上的属性
+								options: {
+									max: 100,
+								}
+							},
+							{
+								field: 'combo_rangeEnd',
+								options: {
+									max: 100,
+								}
+							}
+						]
 					}
+					// {
+					// 	type: 'rangeDatePicker',
+					// 	field: 'selectComboFilter',
+					// }
 				]
 			}
 		]);
