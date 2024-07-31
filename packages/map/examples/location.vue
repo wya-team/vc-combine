ba04da07a6a99b975ef16a3e89ba748e053ad374
Mr.Z <zrd0921@qq.com>
2024-08-01 06:56:16 +0800

    chore: amap@v2 config by self

--- packages/map/examples/location.vue
+++ packages/map/examples/location.vue
@@ -9,6 +9,15 @@
 import { ref, watch } from 'vue';
 import Map from '../index.ts';
 
+// Map.Manager.inject({
+// 	mapScript: '//webapi.amap.com/maps?v=2.0&key=yourkey',
+// 	mapUIScript: '//webapi.amap.com/ui/1.1/main.js?v=1.1.11'
+// });
+
+// window._AMapSecurityConfig = {
+// 	securityJsCode: "yoursecret",
+// };
+
 const VccMapLocation = Map.Location;
 const lnglat = ref([120.157835, 30.287163]);
 const handleChange = (v, r) => {
